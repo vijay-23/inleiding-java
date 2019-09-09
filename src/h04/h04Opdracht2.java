18599b69748ba817c22e058cbad879740f7755ae
Vijay <vjhoelas@gmail.com>
2019-09-09 09:14:31 +0200

    h4opdracht2

--- src/h04/h04Opdracht2.java
+++ src/h04/h04Opdracht2.java
@@ -11,6 +11,14 @@ public class h04Opdracht2 extends Applet {
     public void paint(Graphics g) {
         setBackground(Color.white);
         g.setColor(Color.black);
+        g.drawLine(100,100, 175, 25);
+        g.drawLine(250, 100, 175, 25);
+        g.drawRect(100,100,150,100);
+        g.drawRect(160,160,25,40);
+        g.drawRect(115,160,30,30);
+        g.drawRect(205,160,30,30);
+        g.fillOval(180,180,5,5);
+
 
 
     }
