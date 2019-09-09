efc5aec24a9cd1b5c34fc991bd2a6748a42ed4c6
Vijay <vjhoelas@gmail.com>
2019-09-09 12:59:02 +0200

    h04Opdracht6

---
+++ src/h04/h04Opdracht6.java
@@ -0,0 +1,17 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class h04Opdracht6 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.black);
+        g.setColor(Color.yellow);
+        g.fillArc(40, 40, 300, 150, 0, 360);
+    }
+}
