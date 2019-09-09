22689571f4c859dae8cc0e148d69b45c13fdd33f
Vijay <vjhoelas@gmail.com>
2019-09-09 14:40:09 +0200

    h04Opdracht6

--- src/h04/h04Opdracht6.java
+++ src/h04/h04Opdracht6.java
@@ -9,9 +9,19 @@ public class h04Opdracht6 extends Applet {
     }
 
     public void paint(Graphics g) {
-        setBackground(Color.blue);
+        setBackground(Color.white);
         g.setColor(Color.black);
+        g.fillRoundRect(60,10,95,205,100,120);
+
+        g.drawRect(92,210,30,80);
+
+        g.setColor(Color.red);
+        g.fillOval(81,23,53,53);
+
         g.setColor(Color.yellow);
-        g.fillArc(40, 40, 300, 150, 0, 360);
+        g.fillOval(81,83,53,53);
+
+        g.setColor(Color.green);
+        g.fillOval(81,143,53,53);
     }
 }
