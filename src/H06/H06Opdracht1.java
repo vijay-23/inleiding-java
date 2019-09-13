020b63ed386591514d0eee301a145434034d72b3
Vijay <vjhoelas@gmail.com>
2019-09-13 11:23:12 +0200

    H06Opdracht1

---
+++ src/H06/H06Opdracht1.java
@@ -0,0 +1,28 @@
+package H06;
+
+import java.awt.*;
+import java.applet.*;
+
+public class H06Opdracht1 extends Applet {
+
+    double Jan, Ali,Jeannette,Vijay,Totaalbedrag;
+
+
+    public void init() {
+        Jan = Totaalbedrag/4;
+        Ali = Totaalbedrag/4;
+        Jeannette = Totaalbedrag/4;
+        Vijay = Totaalbedrag/4;
+
+        Totaalbedrag = 113;
+
+
+    }
+    public void paint(Graphics g) {
+        g.drawString("Totaalbedrag = " + Totaalbedrag, 30, 20);
+        g.drawString("Jan = "+ Totaalbedrag/4,30,50);
+        g.drawString("Ali = "+ Totaalbedrag/4,30,80);
+        g.drawString("Jeannette = "+ Totaalbedrag/4,30,110);
+        g.drawString("Vijay = "+ Totaalbedrag/4,30,140);
+    }
+}
