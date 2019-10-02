0c8af8769100a226eb6d0d851b8a41d6e3efa08d
Vijay <vjhoelas@gmail.com>
2019-10-02 09:11:00 +0200

    H11Opdracht1

---
+++ src/H11/H11Opdracht1.java
@@ -0,0 +1,22 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H11Opdracht1 extends Applet {
+
+
+    public void paint(Graphics g) {
+        g.drawLine(20,20,20,200);
+        g.drawLine(40,20,40,200);
+        g.drawLine(60,20,60,200);
+        g.drawLine(80,20,80,200);
+        g.drawLine(100,20,100,200);
+        g.drawLine(120,20,120,200);
+        g.drawLine(140,20,140,200);
+        g.drawLine(160,20,160,200);
+        g.drawLine(180,20,180,200);
+        g.drawLine(200,20,200,200);
+
+    }
+}
