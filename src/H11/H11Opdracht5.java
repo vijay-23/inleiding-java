691e353f52269d1c00c9de7fe7aeef89086da027
Vijay <vjhoelas@gmail.com>
2019-10-08 12:45:23 +0200

    H11Opdracht5

---
+++ src/H11/H11Opdracht5.java
@@ -0,0 +1,26 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H11Opdracht5 extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int teller;
+        int y = 5;
+        int x = 5;
+        int width = 20;
+        int height = 20;
+
+
+        for (teller = 0; teller < 5; teller++) {
+            g.drawRect(x,y,width,width);
+
+            y += height;
+            x += width;
+
+        }
+    }
+}
---
+++ src/H11/H11Opdracht6.java
@@ -0,0 +1,9 @@
+package H11;
+
+import java.applet.Applet;
+
+public class H11Opdracht6 extends Applet {
+
+
+
+}
