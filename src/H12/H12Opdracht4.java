b3469f19f1840d87196b680392d99c70ba3198b4
Vijay <vjhoelas@gmail.com>
2019-10-30 10:00:11 +0100

    H12Opdracht4

---
+++ src/H12/H12Opdracht4.java
@@ -0,0 +1,56 @@
+package H12;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class H12Opdracht4 extends Applet {
+
+    double[] numbers = { 100, 200, 500, 400, 300 };
+    double searching;
+    Button ok;
+    TextField textField;
+    Boolean found;
+
+    public void init() {
+        searching = 0;
+        found = false;
+        int counter = 0;
+        while (counter < numbers.length) {
+            if (numbers[counter] == searching) {
+                found = true;
+            }
+            counter ++;
+        }
+        textField = new TextField(5);
+        add(textField);
+        ok = new Button("OK");
+        ok.addActionListener(new ButtonListener());
+        add(ok);
+    }
+
+    class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            found = false;
+            searching = Integer.valueOf(textField.getText());
+
+            int counter = 0;
+            while (counter < numbers.length) {
+                if (numbers[counter] == searching) {
+                    found = true;
+                }
+                counter ++;
+            }
+            repaint();
+        }
+    }
+    public void paint(Graphics g) {
+        if(found == true) {
+            g.drawString("De waarde is gevonden.", 20, 50);
+        }
+        else {
+            g.drawString("De waarde is niet gevonden.", 20, 50);
+        }
+    }
+}
